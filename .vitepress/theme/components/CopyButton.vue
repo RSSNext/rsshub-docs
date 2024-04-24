250604c98c2ffc70a101d02cef12d4ca32c92920
TinsFox <fox@tinsfox.com>
2024-04-24 14:15:02 +0800

    feat: copy example url

---
+++ .vitepress/theme/components/CopyButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="copy" class="copy">
+    <svg v-if="!copied" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy">
+      <rect width="14" height="14" x="8" y="8" rx="2" ry="2"/>
+      <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/>
+    </svg>
+    <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-check">
+      <path d="m12 15 2 2 4-4"/>
+      <rect width="14" height="14" x="8" y="8" rx="2" ry="2"/>
+      <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/>
+    </svg>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps,ref } from 'vue';
+
+const props = defineProps<{
+  text: string,
+}>();
+const copied = ref(false);
+const copy = () => {
+  navigator.clipboard.writeText(props.text).then(()=>{
+    copied.value = true;
+    setTimeout(() => {
+      copied.value = false;
+    }, 2000);
+  });
+};
+</script>
